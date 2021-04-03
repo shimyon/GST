a4a3d1648d4ddd2377ad3796e909f48d926cd4f5
shimyon.parmar <shimyon.parmar@scrumbees.com>
2021-04-03 15:37:57 +0530

    Clear repository

--- src/services/ParamLocs.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-    providedIn: 'root'
-})
-export class ParamLocs {
-   Param: any; // of course replace any with a nice interface of your own
- }
