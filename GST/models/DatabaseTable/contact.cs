374fb14242da0127efdb0e33f2f71a3093f88fdf
bhurabhai07 <bhurabhai07@gmail.com>
2021-07-07 21:32:05 +0530

    another changes

---
+++ GST/models/DatabaseTable/contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace models.DatabaseTable
+{
+    [Table("contact")]
+    public class contact : BaseEntity
+    {
+        [StringLength(10)]
+        public string CompanyID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Phone1 { get; set; }
+
+        public string Phone2 { get; set; }
+
+        public string Email { get; set; }
+    }
+}
