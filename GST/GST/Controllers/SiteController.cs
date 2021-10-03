2952bee61ab8b89d07278c393fb32556903f5cf1
shimyon <shimyon@hotmail.com>
2021-10-03 18:34:31 +0530

    Logo upload changes

--- GST/GST/Controllers/SiteController.cs
+++ GST/GST/Controllers/SiteController.cs
@@ -6,6 +6,8 @@ using services.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 namespace GST.Controllers
@@ -48,6 +50,39 @@ namespace GST.Controllers
             siteobj.CreatedBy = authdet.UserId;
             var result = service.Add(siteobj);
             return Ok(result);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage UploadLogo()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    int hasheddate = DateTime.Now.GetHashCode();
+                    //Good to use an updated name always, since many can use the same file name to upload.
+                    string changed_name = hasheddate.ToString() + "_" + postedFile.FileName;
+                    changed_name = "logo.png";
+                    var filePath = HttpContext.Current.Server.MapPath("~/Images/" + changed_name);
+                    postedFile.SaveAs(filePath); // save the file to a folder "Images" in the root of your app
+
+                    changed_name = @"~\Images\" + changed_name; //store this complete path to database
+                    docfiles.Add(changed_name);
+
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            return result;
+
         }
     }
 
